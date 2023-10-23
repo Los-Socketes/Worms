0ff4ca528d1d07bb61d8c7033ead58c8d43d27df
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2023-10-23 12:07:21 -0300

    Anado al header file la lista de escenariosdisponibles, la lista de partidas y funciones para obtenerlas
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- src/server/server.h
+++ src/server/server.h
@@ -1,13 +1,18 @@
 #ifndef SERVER_HEADER
 #define SERVER_HEADER
 
-#include "recibidor.h"
+// #include "recibidor.h"
+#include "partida.h"
 #include "socket.h"
 
 #include "cliente.h"
+#include <cstdint>
+#include <string>
+#include <vector>
 
-//Hago la declaracion para que compile. Cuando este implentado borro
-//esto
+struct RepresentacionPartida {
+    std::string ID;
+};
 
 class Server {
 private:
@@ -15,6 +20,18 @@ private:
     Socket aceptador;
     
     void recibirCliente();
+
+    //Esto lo hago constante porque solo es fijo. Se tiene que leer de
+    //un archivo
+    const std::vector<std::string> escenariosDisponibles;
+
+    // TODO: Preguntar si esto no es una race condition.
+    // Entiendo que si lo es porque se va a modificar en tiempo de
+    // corrida y va a haber threads leyendo y escribiendo en ella.
+    std::vector<Partida> partidas;
+    //Este counter va a hacer de ID para la partida
+    uint partidaCounter;
+    [[nodiscard]] std::vector<RepresentacionPartida> getRepresentacionPartidas();
 
 public:
     Server(const char *puerto);
