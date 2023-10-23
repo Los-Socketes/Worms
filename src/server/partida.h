8b10aa5e7d4882ecf4a063fc1cf5c0eb2be6597d
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2023-10-23 12:04:01 -0300

    Creo la clase partida
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

---
+++ src/server/partida.h
@@ -0,0 +1,12 @@
+#ifndef PARTIDA_HEADER
+#define PARTIDA_HEADER
+
+#include "jugador.h"
+#include <sys/types.h>
+#include <vector>
+
+class Partida {
+    std::vector<Jugador> jugadores;
+};
+
+#endif
