619f8cb8a4bc9b2ff78c0b41ef1ecc48477c8ffb
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2023-10-23 14:03:34 -0300

    Anado mini comentario de por que uso std::vector

--- src/server/server.h
+++ src/server/server.h
@@ -28,6 +28,10 @@ private:
     // TODO: Preguntar si esto no es una race condition.
     // Entiendo que si lo es porque se va a modificar en tiempo de
     // corrida y va a haber threads leyendo y escribiendo en ella.
+    // En teoria, un vector es mas ineficiente que una lista porque
+    // tiene que realocar TODOS los elementos cada vez que se saca
+    // un elemento. Sin embargo, el realocar constantemente
+    // nos asegura que no va a haber espacios vacios en el medio
     std::vector<Partida> partidas;
     //Este counter va a hacer de ID para la partida
     uint partidaCounter;
