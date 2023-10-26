3573123f6fff7763968401cc72beabe3f27dc807
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2023-10-26 20:26:25 -0300

    Comienzo del game loop
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- src/server/partida.cpp
+++ src/server/partida.cpp
@@ -2,3 +2,34 @@
 
 Partida::Partida() {
 }
+
+/*
+  struct zonaAfectada {
+        std::pair<posX,posY> coordenada;
+        int lejania;
+        int danoBase;
+  }
+
+ */
+
+
+void Partida::gameLoop() {
+    int posJugadorActual = 0;
+
+    while (true) {
+        Jugador& jugadorActual = jugadores.at(posJugadorActual);
+        Gusano& gusanoActual = jugadorActual.getGusanoActual();
+
+        //TODO: Cambiar a algo mas generico cuando tengamos las armas
+        // int jugadorActual.getAccion();
+
+        //TODO: Implementar "calcular cambios"
+        //std::list<std::pair<posX,posY> areasAfectadas = this.calcularCambios(Accion);
+
+        //TODO: Implementar "actualizar"
+        //this.actualizar();
+
+
+        posJugadorActual += 1;
+    }
+};
--- src/server/partida.h
+++ src/server/partida.h
@@ -1,20 +1,35 @@
 #ifndef PARTIDA_HEADER
 #define PARTIDA_HEADER
 
+#include "gusano.h"
 #include "jugador.h"
 #include "protocolo.h"
+#include "thread.h"
 #include <cstdint>
 #include <sys/types.h>
+#include <utility>
 #include <vector>
+#include "comunes.h"
 
-class Partida {
+//El game loop ES nuestra funcion run
+#define gameLoop run
+
+enum class Accion { Mover, Saltar, Disparar };
+
+class Partida : public Thread {
     int32_t id;
 
     std::vector<Jugador> jugadores;
+
+    std::vector<Gusano*> gusanos;
+
+    void turnoJugador();
 public:
     Partida();
 
     void anadirJugador(Protocolo&& jugadorNuevo);
+
+    void gameLoop();
 };
 
 #endif
