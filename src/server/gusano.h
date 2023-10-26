1cc8918e351b98a11325c3985ba112f02f0c0da7
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2023-10-26 17:50:16 -0300

    Incluyo clase gusano
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

---
+++ src/server/gusano.h
@@ -0,0 +1,18 @@
+#ifndef GUSANO_HEADER
+#define GUSANO_HEADER
+
+#include <sys/types.h>
+
+// hp = healt points
+typedef uint hp;
+
+class Gusano {
+private:
+    hp vida;
+
+public:
+};
+
+
+
+#endif
