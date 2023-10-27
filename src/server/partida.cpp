4f09ff14e1703850e2538d8ec738ea77418f22c5
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2023-10-26 22:25:52 -0300

    Ahora compila y esta todo Faltaba anadirlo al CMake.
    JAJAJAJJAJAJAJAJAJJAJA QUE GRACIOSO JAJAJJJ GRACIAS CMAKE JAJAJAJJAJAJAJAJAJJAJA
    ARCHIVO A ARCHIVO DECIAN JAJAJAJJAJAJAJAJAJJAJA
    no.
    
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- src/server/partida.cpp
+++ src/server/partida.cpp
@@ -19,22 +19,22 @@ void Partida::anadirJugador(Jugador *jugadorNuevo) {
 
 
 void Partida::gameLoop() {
-    int posJugadorActual = 0;
+    // int posJugadorActual = 0;
 
-    while (true) {
-        Jugador& jugadorActual = jugadores.at(posJugadorActual);
-        Gusano& gusanoActual = jugadorActual.getGusanoActual();
+    // while (true) {
+    //     Jugador& jugadorActual = jugadores.at(posJugadorActual);
+    //     Gusano& gusanoActual = jugadorActual.getGusanoActual();
 
-        //TODO: Cambiar a algo mas generico cuando tengamos las armas
-        // int jugadorActual.getAccion();
+    //     //TODO: Cambiar a algo mas generico cuando tengamos las armas
+    //     // int jugadorActual.getAccion();
 
-        //TODO: Implementar "calcular cambios"
-        //std::list<std::pair<posX,posY> areasAfectadas = this.calcularCambios(Accion);
+    //     //TODO: Implementar "calcular cambios"
+    //     //std::list<std::pair<posX,posY> areasAfectadas = this.calcularCambios(Accion);
 
-        //TODO: Implementar "actualizar"
-        //this.actualizar();
+    //     //TODO: Implementar "actualizar"
+    //     //this.actualizar();
 
 
-        posJugadorActual += 1;
-    }
+    //     posJugadorActual += 1;
+    // }
 };
