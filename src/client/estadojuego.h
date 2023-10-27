a375e0cbc62b564b6fe4135b0e40a8be5dfb13a2
JuanPF56 <jpfresia@fi.uba.ar>
2023-10-27 03:07:23 -0300

    Preparación de protocolo y menú de partidas
    Se armó un pequeño menú que muestra mapas para crear partidas o partidas para unirse a ellas.
    Se establecieron algunas llamadas al protocolo.
    Los cambios están comentados, aún falta probar con conexión a servidor.

--- src/client/estadojuego.h
+++ src/client/estadojuego.h
@@ -2,10 +2,13 @@
 #define ESTADOJUEGO_H_
 
 struct EstadoJuego {
+    int32_t id;
     int coord_x;
     int coord_y;
 
-    EstadoJuego(int coord_x, int coord_y) : coord_x(coord_x), coord_y(coord_y) {}
+    EstadoJuego(int coord_x, int coord_y) : coord_x(coord_x), coord_y(coord_y) {
+        id = 0;
+    }
     
 };
 
--- src/client/recibidor.h
+++ src/client/recibidor.h
@@ -8,13 +8,16 @@
 #include "estadojuego.h"
 #include "../common/thread.h"
 #include "../common/queue.h"
+#include "../common/protocolo.h"
 
 class Recibidor: public Thread {
 private:
+    // Protocolo& protocolo;
     Queue<EstadoJuego>& recepcion_estados;
     std::atomic<bool> cont;
 
 public:
+    // Recibidor(Protocolo& protocolo, Queue<EstadoJuego>& recepcion_estados);
     Recibidor(Queue<EstadoJuego>& recepcion_estados);
 
     // Hilo que se encarga de recibir los estados del juego.
