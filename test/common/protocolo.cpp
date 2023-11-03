1b9fbdc60777fb3a97a368eaa5521205f8aa60c2
antopell <antopell29@gmail.com>
2023-11-02 22:06:58 -0300

    Intento de test, es fea la estructura, pero funciona

--- test/common/protocolo.cpp
+++ test/common/protocolo.cpp
@@ -1,9 +1,28 @@
 #include "protocolo.h"
+#include "socket.h"
 #include <iostream>
 
+
+
 int main() {
+    // MUY FEO, pero es algo que funciona
+    Socket mockSocketServer("9097");
+    Socket mockSocket("127.0.0.1", "9097");
+    Protocolo protocolo(std::move(mockSocket));
+    Socket accept = mockSocketServer.accept();
+    Protocolo protocoloServer(std::move(accept));
+
     std::cout << "Inicio test de protocolo\n";
 
+    // return 0;
+    protocolo.pedirInformacion(MAPA);
+    tipoInfo recibido = protocoloServer.obtenerPedido();
+    if (recibido == MAPA) {
+        return 0;
+    } else {
+        std::cerr << recibido;
+        return 1;
+    }
     // return -1;
     // TODO: agregar tests :p
 }
