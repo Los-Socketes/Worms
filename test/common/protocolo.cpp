cb67cbec55fea671f8b5438e2f8232629ac66cf9
antopell <antopell29@gmail.com>
2023-11-03 21:13:19 -0300

    Agrego integracion con catch2 + primer test funcional con la libreria

--- test/common/protocolo.cpp
+++ test/common/protocolo.cpp
@@ -2,31 +2,51 @@
 #include "socket.h"
 #include <iostream>
 
+#include <catch2/catch.hpp>
 
+#include <cstdint>
 
-int main() {
-    // MUY FEO, pero es algo que funciona
+tipoInfo pedirInformacion(tipoInfo info) {
     Socket mockSocketServer("9097");
     Socket mockSocket("127.0.0.1", "9097");
     Protocolo protocolo(std::move(mockSocket));
     Socket accept = mockSocketServer.accept();
     Protocolo protocoloServer(std::move(accept));
 
-    std::cout << "Inicio test de protocolo\n";
-
-    // TESTEO PEDIR INFORMACION
-    protocolo.pedirInformacion(MAPA);
-    tipoInfo recibido = protocoloServer.obtenerPedido();
-    if (recibido != MAPA) {
-        std::cerr << recibido;
-        return 1;
-    }
-
-    protocolo.pedirInformacion(PARTIDA);
-    recibido = protocoloServer.obtenerPedido();
-    if (recibido != PARTIDA) {
-        std::cerr << recibido;
-        return 1;
-    }
-    return 0;
+    protocolo.pedirInformacion(info);
+    return protocoloServer.obtenerPedido();
 }
+
+TEST_CASE( "Tests de pedirInforamcion", "[pedirInformacion]" ) {
+    REQUIRE( pedirInformacion(MAPA) == MAPA);
+    REQUIRE( pedirInformacion(PARTIDA) == PARTIDA);
+}
+
+
+// int main() {
+//     Socket mockSocketServer("9097");
+//     Socket mockSocket("127.0.0.1", "9097");
+//     Protocolo protocolo(std::move(mockSocket));
+//     Socket accept = mockSocketServer.accept();
+//     Protocolo protocoloServer(std::move(accept));
+
+//     std::cout << "Inicio test de protocolo\n";
+
+
+
+//     // TESTEO PEDIR INFORMACION
+//     protocolo.pedirInformacion(MAPA);
+//     tipoInfo recibido = protocoloServer.obtenerPedido();
+//     if (recibido != MAPA) {
+//         std::cerr << recibido;
+//         return 1;
+//     }
+
+//     protocolo.pedirInformacion(PARTIDA);
+//     recibido = protocoloServer.obtenerPedido();
+//     if (recibido != PARTIDA) {
+//         std::cerr << recibido;
+//         return 1;
+//     }
+//     return 0;
+// }
